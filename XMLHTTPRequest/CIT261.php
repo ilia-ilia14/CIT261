1cfc60edc609dd49d6aa31bb0691c85646fa900a
ilia-ilia14 <simon.ilia.sd@gmail.com>
2018-10-11 01:37:46 -0600

    xhttp fix

---
+++ XMLHTTPRequest/CIT261.php
@@ -0,0 +1,19 @@
+<?php
+   echo '[{
+		"fname": "John",
+		"lname": "Doe",
+		"age": "25",
+		"email": "doe@gmail.com"
+	},
+	{
+		"fname": "James",
+		"lname": "Dean",
+		"age": "37"
+	},
+	{
+		"fname": "Bob",
+		"lname": "Menkok",
+		"age": "35"
+	}
+]';
+?>
